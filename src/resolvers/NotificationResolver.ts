2837022d02fb0c1874e3d2db42f33c969f8879ab
ssook <cesces333@naver.com>
2021-06-19 16:21:58 +0900

    [feat] 읽지않은 노티 개수 api 구현

--- src/resolvers/NotificationResolver.ts
+++ src/resolvers/NotificationResolver.ts
@@ -4,6 +4,9 @@ import { Account } from '../entities/Account';
 import { Notification } from '../entities/Notification';
 import { AuthMiddleware } from '../middleware/typegraphql/auth';
 import { NotificationService } from '../services/NotificationService';
+import { NotiCount } from '../schemas/UnreadNotiCount';
+import BaseError from '../exceptions/BaseError';
+import { ERROR_CODE } from '../exceptions/ErrorCode';
 
 @Service()
 @Resolver(() => Notification)
@@ -16,5 +19,19 @@ export class NotificationResolver {
         const notifications = await this.notificationService.getNotificationsByUser(account);
         console.log(notifications);
         return notifications;
+    }
+
+    @Query((returns) => NotiCount)
+    @UseMiddleware(AuthMiddleware)
+    async getUnreadNotiCount(@Ctx('account') account: Account): Promise<NotiCount> {
+        console.log(account);
+        if (!account) {
+            throw new BaseError(ERROR_CODE.UNAUTHORIZED);
+        }
+        const unreadNotiCount = await this.notificationService.getUnreadNotiCount(account);
+
+        const notiCount = { count: unreadNotiCount };
+
+        return notiCount;
     }
 }
