2e98b51479860c56ddb11fa1f37b2a7f94056f29
Lee-KyungSeok <leeseok8347@gmail.com>
2021-09-14 00:05:48 +0900

    env script 변경

--- script/ssm_parameter_store_to_env.sh
+++ script/ssm_parameter_store_to_env.sh
@@ -3,26 +3,12 @@
 
 deployDir=$1
 
-#function json_extract {
-#  local key=$1
-#  local json=$2
-#
-#  local string_regex='"([^"\]|\\.)*"'
-#  local number_regex='-?(0|[1-9][0-9]*)(\.[0-9]+)?([eE][+-]?[0-9]+)?'
-#  local value_regex="${string_regex}|${number_regex}|true|false|null"
-#  local pair_regex="\"${key}\"[[:space:]]*:[[:space:]]*(${value_regex})"
-#
-#  if [[ ${json} =~ ${pair_regex} ]]; then
-#    echo $(sed 's/^"\|"$//g' <<< "${BASH_REMATCH[1]}")
-#  else
-#    return 1
-#  fi
-#}
+set -e
+
+
 
 ssmValue=$(aws ssm get-parameter --name hush-api-server-env)
-#echo $ssmValue | jq '.Parameter.Value'
-stringValue=$(echo $ssmValue | jq '.Parameter.Value' | sed 's/\"//g')
-#stringValue=$(json_extract Value "${ssmValue}" | sed 's/\"//g')
+stringValue=$(echo $ssmValue | tr '\r\n' ' ' | jq '.Parameter.Value' | sed 's/\"//g')
 
 echo $stringValue | sed 's/\\n/\
 /g' > $deployDir
