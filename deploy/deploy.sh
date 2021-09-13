ee18f0e5ce378ca9fc4276e5f20ebc7198490ef1
Lee-KyungSeok <leeseok8347@gmail.com>
2021-09-14 03:44:37 +0900

    image delete script 추가

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "deploy started...."
+function dkrm(){
+  DOCKER_IMAGE_IDS=$(docker images | grep "none" | awk '{print $3}')
+  if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
+    echo "========== No images available for deletion ==========="
+  else
+    docker rmi -f $DOCKER_IMAGE_IDS
+  fi
+}
