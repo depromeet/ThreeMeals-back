43dc05112a54920d3327854f294e1238ab184627
Lee-KyungSeok <leeseok8347@gmail.com>
2021-09-14 01:18:52 +0900

    env script 변경 3 (세번째 우회)

--- script/ssm_parameter_store_to_env.sh
+++ script/ssm_parameter_store_to_env.sh
@@ -8,7 +8,15 @@ ssmValue=$(aws ssm get-parameter --name hush-api-server-env)
 #stringValue=$(echo $ssmValue | tr '\r\n' ' ' | jq '.Parameter.Value' | sed 's/\"//g')
 stringValue=$(echo $ssmValue | tr '\r\n' ' ' | jq '.Parameter.Value' | sed 's/\"//g' | sed 's/\\n/,/g')
 
-echo $stringValue  | tr ',' '\n' > $deployDir
+touch $deployDir
+
+#echo $stringValue  | tr ',' '\n' > $deployDir
+
+IFS=","
+for v in $stringValue
+do
+   echo $v >> $deployDir
+done
 
 #echo $stringValue | sed 's/\\n/\
 #/g' > $deployDir
