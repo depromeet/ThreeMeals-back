5b5882e576aa5913623e61bf122c920f03b186c3
Lee-KyungSeok <leeseok8347@gmail.com>
2021-09-21 17:58:42 +0900

    e2e 테스트 버그 수정

--- src/infrastructure/apollo/middleware/error.ts
+++ src/infrastructure/apollo/middleware/error.ts
@@ -1,11 +1,11 @@
-import { Service } from 'typedi';
-import { MiddlewareInterface, NextFn, ResolverData } from 'type-graphql';
+import { MiddlewareFn } from 'type-graphql';
 import { BaseError, IBaseError } from '../../../domain/exceptions/BaseError';
 import { ERROR_CODE } from '../../../domain/exceptions/ErrorCode';
-import { AuthContext } from '../../express/middlewares/AuthContext';
-import { Logger } from '../../typedi/decorator/Logger';
 import { ILogger } from '../../logger/ILogger';
 import { ApolloError } from 'apollo-server-express';
+import { JsonLogger } from '../../logger/JsonLogger';
+
+const logger: ILogger = new JsonLogger('ErrorInterceptor');
 
 export const errorToBaseError = (error: Error): IBaseError => {
     return new BaseError({
@@ -15,37 +15,37 @@ export const errorToBaseError = (error: Error): IBaseError => {
     });
 };
 
-@Service()
-export class ErrorInterceptor implements MiddlewareInterface<AuthContext> {
-    constructor(
-        @Logger() private readonly logger: ILogger,
-    ) {
-    }
+export const ErrorInterceptor: MiddlewareFn<any> = async ({ context, info }, next) => {
+    try {
+        return await next();
+    } catch (err) {
+        const baseError = err ?
+            err instanceof BaseError ?
+                err :
+                errorToBaseError(err) :
+            new BaseError({
+                errorCode: ERROR_CODE.INTERNAL_SERVER_ERROR,
+                message: 'unknown error, please check error',
+            });
 
-    async use({ context, info }: ResolverData<AuthContext>, next: NextFn) {
-        try {
-            return await next();
-        } catch (err) {
-            const baseError = err ?
-                err instanceof BaseError ?
-                    err :
-                    errorToBaseError(err) :
-                new BaseError({
-                    errorCode: ERROR_CODE.INTERNAL_SERVER_ERROR,
-                    message: 'unknown error, please check error',
-                });
-
-            // write error to file log
-            if (baseError.logMessage || baseError.errorData) {
-                this.logger.error(
-                    baseError.logMessage ? baseError.logMessage : baseError.message,
-                    baseError.errorData,
-                    baseError.stack,
-                );
-            }
-
-            // rethrow the error
-            throw new ApolloError(baseError.message, baseError.code);
+        // write error to file log
+        if (baseError.logMessage || baseError.errorData) {
+            logger.error(
+                baseError.logMessage ? baseError.logMessage : baseError.message,
+                baseError.errorData,
+                baseError.stack,
+            );
         }
+
+        // rethrow the error
+        throw new ApolloError(baseError.message, baseError.code);
     }
-}
+};
+
+// export class ErrorInterceptor implements MiddlewareInterface<AuthContext> {
+//     private readonly logger: ILogger = new JsonLogger('ErrorInterceptor');
+//
+//     async use({ context, info }: ResolverData<AuthContext>, next: NextFn) {
+//
+//     }
+// }
--- test/e2e/account/account.spec.ts
+++ test/e2e/account/account.spec.ts
@@ -4,6 +4,7 @@ import * as nock from 'nock';
 import * as faker from 'faker';
 import { ProviderType } from '../../../src/domain/aggregates/account/ProviderType';
 import { KAKAO_URL } from '../../../src/infrastructure/provider-user/KakaoApi';
+import { ERROR_CODE } from '../../../src/domain/exceptions/ErrorCode';
 
 describe('AccountResolver', () => {
     let app: TestApp;
@@ -32,7 +33,7 @@ describe('AccountResolver', () => {
             };
         });
 
-        it('카카오 로그인에 성공하면 토큰을 발행한다.', async () => {
+        it('카카오 로그인에 성공하면 토큰을 발행한다.', async (done) => {
             // given
             nock(KAKAO_URL.BASE_PATH)
                 .get(KAKAO_URL.ME)
@@ -57,6 +58,37 @@ describe('AccountResolver', () => {
             // then
             expect(status).toBe(200);
             expect(body.data.signIn.token).toBeDefined();
+            done();
+        });
+
+        it('카카오 토큰 형식이 알맞지 않다면 kakao 로그인 에러를 반환한다.', async (done) => {
+            // given
+            nock(KAKAO_URL.BASE_PATH)
+                .get(KAKAO_URL.ME)
+                .reply(401, undefined);
+            const variables = {
+                accessToken: 'abc',
+                provider: ProviderType.Kakao,
+            };
+            const query = `
+            mutation signIn($accessToken: String!, $provider: String!) {
+                signIn(accessToken: $accessToken, provider: $provider) {
+                    token
+                }
+            }
+            `;
+
+            // when
+            const { status, body } = await request(app.instance)
+                .post('/graphql')
+                .send({ query, variables });
+
+            // then
+            expect(status).toBe(200);
+            expect(body.errors).toHaveLength(1);
+            expect(body.errors[0].extensions.code).toBe(ERROR_CODE.KAKAO_LOGIN_ERROR.code);
+            expect(body.errors[0].message).toBe(ERROR_CODE.KAKAO_LOGIN_ERROR.message);
+            done();
         });
     });
 });
